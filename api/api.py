5cef480f296f864eef5dcf1aebbb9cbd19e0482c
Zachari <150785666+JeCode667@users.noreply.github.com>
2025-04-14 10:38:10 +0200

    Update api.py

--- api/api.py
+++ api/api.py
@@ -166,7 +166,5 @@ def chat():
     """
     return "Chatbot API is running!"
 
-# Lancer l'application Flask
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render fournit $PORT
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
